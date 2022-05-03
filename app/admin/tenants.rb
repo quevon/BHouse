3c764f008d5dd92db9240033f06d17b9c4c02673
quevon <quevonmontojo@gmail.com>
2022-05-03 15:35:35 +0800

    fixed layout of tenants in admin

--- app/admin/tenants.rb
+++ app/admin/tenants.rb
@@ -1,19 +1,42 @@
 ActiveAdmin.register Tenant do
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-   permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
-  
-#   or
-  
-#   permit_params do
-#     permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
-#     permitted << :other if params[:action] == 'create' && current_user.admin?
-#     permitted
-#   end
-  
+  before_action :remove_password_params_if_blank, only: [:update]
+  controller do
+
+    def remove_password_params_if_blank
+      if params[:owner][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:owner].delete(:password)
+        params[:owner].delete(:password_confirmation)
+      end
+    end
+  end
+  filter :email
+  filter :approved
+
+  form do |f|
+    f.inputs do
+        if f.object.new_record?
+            f.input :email
+            f.input :password
+            f.input :password_confirmation
+        else
+            f.input :email
+        end
+    end
+    f.submit
+  end 
+ index do
+        selectable_column
+        id_column
+        column :email
+        column :created_at
+        actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+    end
+  end
 end
