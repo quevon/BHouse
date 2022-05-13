3c65a0627d88aa2b3c695c7a6785a342eb191cef
Jianne Gabriel <jiannegabriel@gmail.com>
2022-05-13 10:30:04 +0800

    Delete env

--- app/javascript/packs/nav.js
+++ app/javascript/packs/nav.js
@@ -1,27 +1,21 @@
-
-
-function activeLink(){
-    const list = document.querySelectorAll('.navigation li')
-    list.forEach((item) =>
-    item.classList.remove('hovered'));
-    this.classList.add('hovered');
+function activeLink() {
+  const list = document.querySelectorAll('.navigation li')
+  list.forEach((item) => item.classList.remove('hovered'))
+  this.classList.add('hovered')
 }
 
-document.addEventListener('turbolinks:load', () => {  
-    const list = document.querySelectorAll('.navigation li')
-    list.forEach((item) =>
-    item.addEventListener('mouseover',activeLink));
-});
+document.addEventListener('turbolinks:load', () => {
+  const list = document.querySelectorAll('.navigation li')
+  list.forEach((item) => item.addEventListener('mouseover', activeLink))
+})
 
-
-
-function hideShowNav() { 
-    const main = document.querySelector('.main');
-    const navigation = document.querySelector('.navigation');
-    navigation.classList.toggle("active");
-    main.classList.toggle("active");
+function hideShowNav() {
+  const main = document.querySelector('.main')
+  const navigation = document.querySelector('.navigation')
+  navigation.classList.toggle('active')
+  main.classList.toggle('active')
 }
-document.addEventListener('turbolinks:load', () => {  
-    const toggles = document.querySelector('.toggle');
-    toggles.addEventListener('click',hideShowNav);
-});
+document.addEventListener('turbolinks:load', () => {
+  const toggles = document.querySelector('.toggle')
+  toggles.addEventListener('click', hideShowNav)
+})
