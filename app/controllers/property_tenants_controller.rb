9c6ea65b8c1237c3ce34c7d4a0b882a1d14679b8
Jianne Gabriel <jiannegabriel@gmail.com>
2022-05-16 18:51:57 +0800

    Tenant send application method and display

--- app/controllers/property_tenants_controller.rb
+++ app/controllers/property_tenants_controller.rb
@@ -10,7 +10,21 @@ class PropertyTenantsController < ApplicationController
           end
     end
 
+    def update
+      @property_tenant = PropertyTenant.find(params[:property_tenant][:id])
+      @property_tenant.status = params[:property_tenant][:status]
+      byebug
+      respond_to do |format|
+          if @property_tenant.update(property_tenant_params)
+            format.html { redirect_to property_url(@property_tenant.property), notice: 'Application Status Updated' }
+          else
+            format.html { redirect_to property_url(@property_tenant.property), notice: 'Error' }
+          end
+        end
+    end
+
     private
+
     def property_tenant_params
         params.require(:property_tenant).permit(:property_id, :tenant_id, :status)
     end
