39e35e844e7dd7cef089cfcc9d10cb9678b72630
quevon <quevonmontojo@gmail.com>
2022-05-19 20:05:53 +0800

    add

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,31 +1,38 @@
 class TransactionsController < InheritedResources::Base
-  before_action :authenticate_owner!
+    # before_action :authenticate_tenant!,only: [:payment]
+  # before_action :authenticate!
+
 
   def index
-    @transactions = Transaction.where( :owner_id => current_owner.id)
+    if current_owner
+     @transactions = Transaction.where( :owner_id => current_owner.id)
+    end
+    if current_tenant
+       @transactions = Transaction.where( :tenant_id => current_tenant.id)
+     end
   end
 
+  def payment
+    # @transactions = Transaction.where( :tenant_id => current_tenant.id)
+  end
+  
   # def authenticate!
   #   if owner_signed_in?
-  #     :authenticate_owner!, :except => 
+  #     :authenticate_owner!
   #   elsif tenant_signed_in?
   #     :authenticate_tenant!
   #   end
   # end
-  def tenant_signed_in?
-    tenant_signed_in?
+  def updatebalance
+    update_balance_owner()
+    update_balance_tenant()
   end
-  def tenant
-    # @transactions = Transaction.where( :tenant_id => current_tenant.id)
-  end
-
   def update_balance_owner
-    @transaction.owner.update!(balance: @transaction.owner.balance + @transaction.amount)
+    current_owner.update!(balance: current_owner.balance + @transaction.amount)
   end
   def update_balance_tenant
-    current_tenant.update!(balance: current_tenant.balance - @transaction.amount)
+    @transaction.tenant.update!(balance:  @transaction.tenant.balance - @transaction.amount)
   end
-
   def create
     @transaction = Transaction.new(transaction_params)
 
@@ -33,8 +40,7 @@ class TransactionsController < InheritedResources::Base
       if @transaction.save
         format.html { redirect_to @transaction, notice: 'Transaction was successfully created.' }
         format.json { render json: @product, status: :created, location: @transaction }
-        update_balance_tenant()
-        update_balance_owner()
+  
       else
         format.html { render action: "new" }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
