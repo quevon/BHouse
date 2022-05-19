d92bfdce8e07ec9c65c0bd9b3b48d32c4230c0e0
Patricia Navarro <navarropatricia15@yahoo.com.ph>
2022-05-18 21:40:19 -0700

    modified style of conversations page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -16,6 +16,26 @@ class ConversationsController < ApplicationController
         else
             @recipients = Owner.all
         end 
+
+        # @conversation = current_user.mailbox.conversations.find(Property.first.owner_id)
+        # @ongoing_conversation = current_user.mailbox.conversations.find(params[:id])
+
+        # if current_owner
+        #     @recipient = Tenant.find(params[:recipient])
+        # else
+        #     @recipient = Owner.find(params[:recipient])
+        # end 
+        # @recipient = @property.owner_id
+        # @ongoing_conversation = Conversation.participant(current_user).participant(@recipient)
+
+        # respond_to do |format|
+        #     format.html { redirect_to root_path }
+        #     if @ongoing_conversation.present?
+        #         format.js { redirect_to mailbox.conversations.find(@ongoing_conversation) }
+        #     else
+        #         format.js
+        #     end
+        # end
     end
 
     def create
@@ -24,7 +44,7 @@ class ConversationsController < ApplicationController
         else
             recipient = Owner.find(params[:owner_id])
         end 
-        receipt = current_user.send_message(recipient, params[:body], params[:subject])
+        receipt = current_user.send_message(recipient, params[:body], "subject")
         redirect_to conversation_path(receipt.conversation)
     end
 end
